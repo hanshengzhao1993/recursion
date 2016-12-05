4d2c876e96f2006c253856c92848e56afc31cf6a
Han <hanshengzhao1993@gmail.com>
2016-12-04 23:04:58 -0800

    Finished StringiftJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,26 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	// if(obj === undefined){return undefined}
+	if(typeof obj === "number" || obj === null || typeof obj === "boolean"){
+		return "" + obj;
+	}if(typeof obj === "string"){
+		return '"' + obj + '"';
+	}if ( Array.isArray(obj) ){
+		var answer = [];
+		for(var i = 0; i < obj.length ; i++){
+			answer.push(stringifyJSON(obj[i]));
+		}
+		return  "[" + answer + "]";
+	}if( typeof obj === "object" && !Array.isArray(obj)){
+		var ans = [];
+		for(var key in obj){
+			if(typeof obj[key] === "undefined" || typeof obj[key] === "function" ){
+				continue;
+			}
+			ans.push(stringifyJSON(key)+ ":" + stringifyJSON(obj[key]))
+		}
+		return "{" + ans + "}";
+	}
+
 };
